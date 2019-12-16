b14af8437ba3735d60861bfa8b2f492c8e536c14
Khani Launggu <kanilaunggu@gmail.com>
2019-12-16 10:35:37 +0700

    Go: Unmarshal Json

---
+++ main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+)
+
+type User struct {
+	Name string `json:"name"`
+	Age  string `json:"age"`
+}
+
+func main() {
+	var JsonString = `{"name":"Khani", "age":"49"}`
+	var JsonData = []byte(JsonString)
+
+	var data User
+
+	var err = json.Unmarshal(JsonData, &data)
+
+	if err != nil {
+		fmt.Println("Error Unmarshalling Json " + err.Error())
+		return
+	}
+
+	fmt.Println(data)
+
+	fmt.Println("Nama : " + data.Name)
+	fmt.Println("Umur : " + data.Age)
+}
