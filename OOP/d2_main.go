8c01923dda95c8d9f20e52b52eb4b0b4444c782f
Khani Launggu <kanilaunggu@gmail.com>
2019-12-17 12:57:53 +0700

    GO: Perbaharui d2_main.go

--- OOP/d2_main.go
+++ OOP/d2_main.go
@@ -1,18 +1,35 @@
 package main
 
 import (
+	"fmt"
+	"oop/blog"
 	"oop/product"
 	"oop/profile"
+	"oop/store"
 )
 
 func main() {
 	//var name = "Khani"
 	profile := profile.User{}
 
-	profile.GetProfile()
 	profile.SetProfile("Khani", "Launggu", "Sulawesi")
+	profile.GetProfile()
+	//profile.SetProfile("Khani", "Launggu", "Sulawesi")
 
 	product := product.New("A", 200)
 	product.GetProduct()
+
+	blog := blog.New("A", "B", "C")
+	blog.GetBlog()
+
+	brancstore := store.BranchStore{StoreName: "Store: Toko apa saja,", OwnerBranch: "Owner: Saya dan Dia"}
+	store := store.Store{BranchStore: brancstore, StoreName: "Toko apa saja 1", Owner: "Saya dan Dia juga"}
+
+	store.GetBranchStore()
+
+	fmt.Println(store)
+
+	//cab := BranchStore{StoreName: "Cab1,", OwnerBranch: "Own1"}
+	//fmt.Println(cab)
 
 }
